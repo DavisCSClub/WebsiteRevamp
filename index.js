64d9b546be40a091188159fb8ae6d85e5e527100
Shivam Desai <shivamdesai@Shivams-MacBook-Air.local>
2019-03-05 22:52:17 -0800

    changed to mongoose

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
 'use strict';
-/*
-window.onload = (function($) {
-    var date = new Date().toDateString().split(' ');
-    var date_string = 'Today ' + date[1] + ' ' + date[2] + ' ' + date[3];
-    const date_string_dom = document.createElement('h1');
-    const calender = document.querySelector('.calender__header');;
-
-    date_string_dom.textContent = 'Today ' + date[1] + ' ' + date[2] + ' ' + date[3];
-    calender.insertBefore(date_string_dom, calender.firstElementChild);
-});
-
-setTimeout(function(){
-    location.reload();
-},10000000);
-*/
 
 const express = require('express');
 const app = express();
 var bodyParser = require('body-parser')
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use( bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
@@ -26,61 +11,69 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 app.use(express.static('assets'));
 app.use(express.static('images'));
 
-const mongodb = require('mongodb');
+const mongoose = require('mongoose');
 const http = require('http');
 const nconf = require('nconf');
 
-let uri = `mongodb://admin:dcsc4lyfe2018@dcsc-shard-00-00-oixna.gcp.mongodb.net:27017,dcsc-shard-00-01-oixna.gcp.mongodb.net:27017,dcsc-shard-00-02-oixna.gcp.mongodb.net:27017/test?ssl=true&replicaSet=DCSC-shard-0&authSource=admin&retryWrites=true`;
-if (nconf.get('mongoDatabase')) {
-  uri = `${uri}/${nconf.get('mongoDatabase')}`;
-}
+let uri = `mongodb://admin:dcsc4lyfe2018@dcsc-shard-00-00-oixna.gcp.mongodb.net:27017,dcsc-shard-00-01-oixna.gcp.mongodb.net:27017,dcsc-shard-00-02-oixna.gcp.mongodb.net:27017/dcsc?ssl=true&replicaSet=DCSC-shard-0&authSource=admin&retryWrites=true`;
+
 console.log(uri);
 
-var timestamp = new Date().getTime();
-var date = new Date(timestamp);
-var date_obj = {
-  month: date.getMonth() + 1,
-  day: date.getDate(),
-  year: date.getFullYear(),
-  hour: date.getHours(),
-  minutes: date.getMinutes(),
-  seconds: date.getSeconds()
-}
+mongoose.connect(uri);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function() {
+  console.log("Connected to database!")
+});
+
+const memberSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    default: ''
+  },
+  lastName: {
+    type: String,
+    default: ''
+  },
+  email: {
+    type: String,
+    default: ''
+  },
+  year: {
+    type: String,
+    default: ''
+  },
+  bitbyte: {
+    type: String,
+    default: ''
+  },
+  source: {
+    type: String,
+    default: ''
+  }
+});
+
+var Member = mongoose.model('Member', memberSchema)
 
 app.post("/", (req, res) => {
 
-  mongodb.MongoClient.connect(uri, (err, db) => {
-    if (err) {
-      throw err;
-    }
-    var dbo = db.db("dcsc");
-    var myobj = {
-      timestamp: date_obj,
-      first_Name: req.body.firstName,
-      last_Name: req.body.lastName,
-      class_year: req.body.year,
-      email: req.body.email,
-      bitbyte_status: req.body.bitbyte,
-      source: req.body.source,
-    };
+  var newMember = new Member(req.body);
 
-  dbo.collection("members").insertOne(myobj, function(err, res) {
-    if (err) throw err;
-    console.log("1 document inserted");
-    db.close();
-  });
+  console.log(newMember)
+  newMember.save(function (err, newMember) {
+    if (err) return console.error(err);
   });
 
   res.sendFile(__dirname + "/index.html")
-
 });
-
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+  res.sendFile(__dirname + "/index.html")
+})
 
-const server = app.listen(8080, () => {
+
+const server = app.listen(8080 | process.env.PORT , () => {
   const host = server.address().address;
   const port = server.address().port;
 
